4ba7e3ae8e1bc97528e8c1edde351364bcac3850
Sushree Sadhana Khatua <sushree.sadhana@gmail.com>
2022-02-23 23:26:04 +0100

    Update User.cs

--- GreetingService.Core/Entities/User.cs
+++ GreetingService.Core/Entities/User.cs
@@ -10,7 +10,7 @@ namespace GreetingService.Core.Entities
     {
         public string? First_name {get; set;}
         public string? Last_name { get; set;}
-        public string? Email { get; set; }
+        public string Email { get; set; }
         public string? Password { get; set; }
         public DateTime Created { get; set; }= DateTime.Now;
         public DateTime Modified { get; set; }=DateTime.Now;
