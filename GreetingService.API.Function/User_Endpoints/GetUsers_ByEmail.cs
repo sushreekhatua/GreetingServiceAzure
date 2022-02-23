ea4b746d3a9789764041e33682a04405bf250ae2
Sushree Sadhana Khatua <sushree.sadhana@gmail.com>
2022-02-23 21:50:49 +0100

    Update GetUsers_ByEmail.cs

--- GreetingService.API.Function/User_Endpoints/GetUsers_ByEmail.cs
+++ GreetingService.API.Function/User_Endpoints/GetUsers_ByEmail.cs
@@ -34,7 +34,7 @@ namespace GreetingService.API.Function.User_Endpoints
 
 
 
-        [FunctionName("GetUsers_ById")]
+        [FunctionName("GetUsers_By_Email")]
         [OpenApiOperation(operationId: "Run", tags: new[] { "User" })]
         // [OpenApiParameter(name: "name", In = ParameterLocation.Query, Required = true, Type = typeof(string), Description = "The **Name** parameter")]
         [OpenApiResponseWithBody(statusCode: HttpStatusCode.OK, contentType: "application/json", bodyType: typeof(IEnumerable<User>), Description = "The OK response")]
